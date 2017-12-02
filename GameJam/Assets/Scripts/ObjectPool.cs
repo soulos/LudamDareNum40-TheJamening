eb04908923ac52c28a81526a6f1369433d63eb3c
Nigel Budden <nigelbudden@savagecabbage.com>
2017-12-02 11:37:06 -0800

    Start of object pooling work

---
+++ GameJam/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ObjectPool
+{
+    public string PoolName;
+
+    public Transform[] Objects;
+}
+
---
+++ GameJam/Assets/Scripts/ObjectPoolingManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ObjectPoolingManager : MonoBehaviour
+{
+
+    public ObjectPool[] ObjectPool;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public static void DestroyPooledObject(string poolName, Transform pooledItem)
+    {
+        
+    }
+}
+
