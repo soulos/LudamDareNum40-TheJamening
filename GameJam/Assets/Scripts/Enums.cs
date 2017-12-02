ea1dde3ef1f7389537144ae4ebc93964405683b1
Nigel Budden <nigelbudden@savagecabbage.com>
2017-12-01 21:15:20 -0800

    Wooo fun and games Core scenes setup Game manager under way

---
+++ GameJam/Assets/Scripts/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace Assets.Scripts
+{
+    public enum GameState
+    {
+        Startup,
+        MainMenu,
+        StartGame,
+        PlayingGame,
+        DiedBullet,
+        DiedAlcohol,
+        DiedZombie,
+        GotHighscore,
+    }
+}
--- GameJam/Assets/Scripts/GameManager.cs
+++ GameJam/Assets/Scripts/GameManager.cs
@@ -1,16 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Assets.Scripts;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
+    private static GameState currentState = GameState.Startup;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
 	// Use this for initialization
 	void Start () {
+
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        
 		
 	}
+
+    public static void ChangeState(GameState newState)
+    {
+        
+        if (currentState == newState)
+        {
+            return;
+        }
+        currentState = newState;
+        switch (newState)
+        {
+            case GameState.Startup:
+                SceneManager.LoadScene("Startup");
+                break;
+            case GameState.MainMenu:
+                SceneManager.LoadScene("MainMenu");
+                break;
+            case GameState.StartGame:
+                SceneManager.LoadScene("Level1");
+                break;
+            case GameState.DiedAlcohol:
+                SceneManager.LoadScene("DeadDrunk");
+                break;
+            case GameState.DiedBullet:
+                SceneManager.LoadScene("DeadBullet");
+                break;
+            case GameState.DiedZombie:
+                SceneManager.LoadScene("DeadZombie");
+                break;
+            case GameState.GotHighscore:
+                SceneManager.LoadScene("HighScore");
+                break;
+            default:
+                break;
+        }
+        
+
+    }
+    
 }
