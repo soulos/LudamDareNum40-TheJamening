2f9590190506fc664ac10cf7fc72f20b88045b51
Nigel Budden <nigelbudden@savagecabbage.net>
2017-12-02 16:51:34 -0800

    Update to pooling can now see the pool object containers almost there

--- GameJam/Assets/Scripts/ObjectPool.cs
+++ GameJam/Assets/Scripts/ObjectPool.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[Serializable]
 public class ObjectPool
 {
+    [SerializeField]
     public string PoolName;
-
+    [SerializeField]
     public Transform[] Objects;
 }
 
