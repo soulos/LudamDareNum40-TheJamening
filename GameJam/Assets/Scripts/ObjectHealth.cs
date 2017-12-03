f7bc19d1e9e7f71c25bba2a5c60fc68b80d541f7
Nigel Budden <nigelbudden@savagecabbage.com>
2017-12-02 18:58:42 -0800

    Zombie spawner and "zombie1" runs at player spawner is a little fast right now but it is configurable :)

--- GameJam/Assets/Scripts/ObjectHealth.cs
+++ GameJam/Assets/Scripts/ObjectHealth.cs
@@ -46,7 +46,7 @@ public class ObjectHealth : MonoBehaviour
                     }
                     else
                     {
-                        
+
                     }
                 }
 
